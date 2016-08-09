51c584724545b640e0ef4158a56809ff06b14519
koztay <koztay@me.com>
2016-08-09 12:25:18 +0300

    Docker adaptasyonu ile ilgili çalışmalar yapıldı.

--- src/orders/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import UserCheckout, UserAddress, Order
-
-admin.site.register(UserCheckout)
-
-admin.site.register(UserAddress)
-
-admin.site.register(Order)
