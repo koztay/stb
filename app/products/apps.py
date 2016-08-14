a8fb56dcc8353fb2c3f697d20e858b419dec0747
koztay <koztay@me.com>
2016-08-14 20:38:03 +0300

    Şu an static files, media files, hepsi çalışır durumda.  Yandaki Before commit kutuları seçili olduğu için kodların anasını bellemiş PyCharm. Şu an yeniden commit ettiğim apps.py dosyasında signal 'lerin çalışmasını sağlayan import kodunu silmiş hayvan, deli etti beni....

--- app/products/apps.py
+++ app/products/apps.py
@@ -8,4 +8,7 @@ class ProductsAppConfig(AppConfig):
     def ready(self):
         # import signal handlers # aşağıdaki satırdaki gibi signals import edilmezse hiçbir
         # signal çalışmıyor...
+        # Birşey daha farkettim eğer commit esnasında refactor, vb. gibi checkbox 'ları
+        # check ettiysen o zaman geri zekalı pycharm benim şağıda import etiğim satırı
+        # siliyor.
         from . import signals
