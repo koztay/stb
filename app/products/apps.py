3488291dbccd5423fd66b5f51f779a4312982abd
koztay <koztay@me.com>
2016-08-14 20:30:03 +0300

    Ayrıca products içinde apps.py içerisinde import signals silinmiş. Hiçbir signal çalışmıyordu. Bu nasıl iş gerçekten anlamadım. Save metodu çalışmıyor mu PyCharm 'da? Sanki bunu save etmeden çıkmışım. Ayrıca defalarca commit ettim. Resmen saçmalık.

--- app/products/apps.py
+++ app/products/apps.py
@@ -8,4 +8,4 @@ class ProductsAppConfig(AppConfig):
     def ready(self):
         # import signal handlers # aşağıdaki satırdaki gibi signals import edilmezse hiçbir
         # signal çalışmıyor...
-        form . import signals
+        from . import signals
