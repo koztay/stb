c7b190af4a5006014866588736cbb7a4ab2e9f2d
koztay <koztay@me.com>
2016-09-09 19:01:12 +0300

    ProductFilterForm sadelestirildi.

--- app/products/forms.py
+++ app/products/forms.py
@@ -11,11 +11,11 @@ CAT_CHOICES = (
 
 class ProductFilterForm(forms.Form):
     # q = forms.CharField(label='Search', required=False)
-    category_id = forms.ModelMultipleChoiceField(
-        label='Category',
-        queryset=Category.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False)
+    # category_id = forms.ModelMultipleChoiceField(
+    #     label='Category',
+    #     queryset=Category.objects.all(),
+    #     widget=forms.CheckboxSelectMultiple,
+    #     required=False)
     # aşağıdakini aktif edince not a valid choices hatası veriyor...
     # category_title = forms.ChoiceField(
     #     label='Category',
@@ -24,7 +24,13 @@ class ProductFilterForm(forms.Form):
     #     required=False)
     max_price = forms.DecimalField(decimal_places=2, max_digits=12, required=False)
     min_price = forms.DecimalField(decimal_places=2, max_digits=12, required=False)
-    tag = forms.CharField(required=False)
+    # tag = forms.CharField(required=False)
+    # hem kategoriye hem de tag 'e  form olarak gerek yok bu form da. Sonuçta tag listi,
+    # object list ten oluşturuyoruz ve taglet tıklandıkça object list daralıyor. Benzer
+    # şekilde kategoriler de tıklandıkça daralan bir yapı mı yapmak lazım anlamadım.
+    # gittigidiyor, yanlarına ürün sayısı yazıp tıklandıkça artan bir yapı seçmiş.
+    # Aslında benzer yapı taglerde de olabilir ama mantıklı değil ki giderek daralması
+    # lazım.
 
 
 class VariationInventoryForm(forms.ModelForm):
