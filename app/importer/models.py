bc12dc5b0cd41ece70f5f9743049cd7f132ff5c7
koztay <koztay@me.com>
2016-10-12 12:39:47 +0300

    oooooffff oofffffff setting dosyalarımı silmiş git

--- app/importer/models.py
+++ app/importer/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+# buraya mapping ile ilgili bir model koyabilirim.
+# sonuçta her firmanın kendine has bir importer 'ı olacak.
--- app/importer/views.py
+++ app/importer/views.py
@@ -20,10 +20,16 @@ class ImporterHomePageView(StaffRequiredMixin, TemplateView):
     template_name = "importer/importer_list.html"
 
 
+# TODO : Önce XLS importu başarılı olarak yap sonra Generic olarak genelle.
 class ProductXLSImporterModel(XLSImporter):
     class Meta:
         model = Product
         ignore_first_line = True
+
+    # process row'u override edeceğiz.
+    # TODO: Burada her Row 'u process ederken task olarak RabbitMQ queue 'ye ekle.
+    def process_row(self, row, values):
+        print(row, values)
 
 
 class ProductXLSXImporterModel(XLSXImporter):
@@ -45,43 +51,6 @@ class XLSImporterCreateView(DataImporterForm):
                          'success_message': "File uploaded successfully"}
         importer = ProductXLSImporterModel
 
-        def form_valid(self, form, owner=None):
-
-            if self.request.user.id:
-                owner = self.request.user
-
-            if self.importer.Meta.model:
-                content_type = ContentType.objects.get_for_model(self.importer.Meta.model)
-                print('cleaned_data:', form.cleaned_data)
-                file_history, _ = FileHistory.objects.get_or_create(file_upload=form.cleaned_data['file_upload'],
-                                                                    owner=owner,
-                                                                    content_type=content_type)
-
-            if not self.is_task or not hasattr(self.task, 'delay'):
-                self.task.run(importer=self.importer,
-                              source=file_history,
-                              owner=owner,
-                              send_email=False)
-                if self.task.parser.errors:
-                    messages.error(self.request, self.task.parser.errors)
-                else:
-                    messages.success(self.request,
-                                     self.extra_context.get('success_message', "File uploaded successfully"))
-            else:
-                self.task.delay(importer=self.importer, source=file_history, owner=owner)
-                if owner:
-                    messages.info(
-                        self.request,
-                        "When importer was finished one email will send to: %s" % owner.email
-                    )
-                else:
-                    messages.info(
-                        self.request,
-                        "Importer task in queue"
-                    )
-
-            return super(DataImporterForm, self).form_valid(form)
-
 
 class XLSXImporterCreateView(DataImporterForm):
         template_name = 'importer/product_importer.html'
@@ -89,10 +58,6 @@ class XLSXImporterCreateView(DataImporterForm):
                          'template_file': 'myfile.xlsx',
                          'success_message': "File uploaded successfully"}
         importer = ProductXLSXImporterModel
-
-        # def form_valid(self, form, owner=None):
-        #     # burada objeleri alıp database'de varsa update edeceğiz.
-        #     return super(DataImporterForm, self).form_valid(form)
 
 
 class XMLImporterCreateView(DataImporterForm):
@@ -102,6 +67,4 @@ class XMLImporterCreateView(DataImporterForm):
                      'success_message': "File uploaded successfully"}
     importer = ProductXMLImporterModel
 
-    # def form_valid(self, form, owner=None):
-    #     # burada objeleri alıp database'de varsa update edeceğiz.
-    #     return super(DataImporterForm, self).form_valid(form)
+
