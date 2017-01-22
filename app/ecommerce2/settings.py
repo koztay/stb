79ec47d91447f011fa706b3f8dcda129df069434
koztay <koztay@me.com>
2017-01-22 19:26:14 +0300

    ilk defa environment variable kullanımında hata oluşumunu çözdüm

--- app/ecommerce2/settings.py
+++ app/ecommerce2/settings.py
@@ -12,30 +12,41 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import environ  # bu çalışırsa yukarıdaki gibi os.getenv kullanımından vazgeçebiliriz.
+
+env = environ.Env()
+env.read_env()
+# print("I am printing the env.read_env", env.read_env())
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # root of project
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='#rw*tsqd36o=78ydz*lv_v2kkzo_5e8gpid512bnx@it0#a9*c')
 
 print("the fucking secret_key is:", SECRET_KEY)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG') does not work
-DEBUG = True
+# DEBUG
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = env.bool('DJANGO_DEBUG', False)
 print("the fucking DEBUG setting is:", DEBUG)
 
 # ALLOWED_HOSTS = ["139.59.139.108", "192.168.99.101", "istebu.com", "www.example.com", ]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=('istebu.com',))
+# print("ALLOWED_HOSTS neymiş bakalım görelim niye set edemiyoruz? =>", ALLOWED_HOSTS)
+# ALLOWED_HOSTS = ('192.168.99.101',)  # bu değeri enviroment variable olarak girince şuna dikkat etmek gerek:
+# Bu parameteryi girerken IP adreslerini tırnak için alma ve virgülle ayırdığın değerlerin arasına
+# space ile boşluk koyma.
+
 
 # EMAIL_HOST = os.environ["EMAIL_HOST"] # bu kesinlikle çalışmıyor.
-EMAIL_HOST = os.getenv('EMAIL_HOST')  # bu çalıştı sanki.
-# EMAIL_HOST = os.environ.get('EMAIL_HOST') # yukarıdaki çalışınca bunun çalışıp çalışmadığını denemedim.
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+# EMAIL_HOST = os.getenv('EMAIL_HOST')  # bu çalıştı sanki.
+# # EMAIL_HOST = os.environ.get('EMAIL_HOST') # yukarıdaki çalışınca bunun çalışıp çalışmadığını denemedim.
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = os.getenv('EMAIL_PORT')
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 
 '''
 If using gmail, you will need to
@@ -81,8 +92,27 @@ INSTALLED_APPS = (
     'vendors',
     'visual_site_elements',
 
-
 )
+
+# Mail settings
+# ------------------------------------------------------------------------------
+EMAIL_PORT = 1025
+EMAIL_HOST = env("EMAIL_HOST", default='mailhog')
+DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
+                         default='istebu <noreply@istebu.com>')
+EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[istebu]')
+SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
+
+# Anymail with Mailgun
+INSTALLED_APPS += ("anymail", )
+ANYMAIL = {
+    "MAILGUN_API_KEY": env('DJANGO_MAILGUN_API_KEY', default='test_api_key'),
+    "MAILGUN_SENDER_DOMAIN": env('MAILGUN_SENDER_DOMAIN', default='test_snder_domain')
+}
+# yukarıda default değerleri yazmamız gerek. Aksi taktirde ilk önce okumaya çalışıp crash oluyor,
+# sonrasında ise docker environment değerlerini set ediyor ve değerler update oluyor.
+
+EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,17 +151,20 @@ WSGI_APPLICATION = 'ecommerce2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-
+# Yine benzer şekilde aşağıya default değerleri yazmazsak docker henüz bu environment
+# variables değerlerini set edemediği için crash kaçınıömaz oluyor.
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_SERVICE'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': env('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': env('DB_NAME', default='default_database'),
+        'USER': env('DB_USER', default='default_user'),
+        'PASSWORD': env('DB_PASS', default='default_password'),
+        'HOST': env('DB_SERVICE', default='localhost'),
+        'PORT': env('DB_PORT', default='1234'),
     },
 }
+
+print (DATABASES)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -189,11 +222,11 @@ CELERY_TIMEZONE = TIME_ZONE
 
 
 # Paynet etc. payment gateway settings will be here
-PAYNET_PUBLISHABLE_KEY = os.getenv('PAYNET_PUBLISHABLE_KEY')
-PAYNET_SECRET_KEY = os.getenv('PAYNET_SECRET_KEY')
-PAYNET_TEST_API_URL = os.getenv('PAYNET_TEST_API_URL')
-PAYNET_TEST_PAYNETJS_URL = os.getenv('PAYNET_TEST_PAYNETJS_URL')
-PAYNET_PRODUCTION_API_URL = os.getenv('PAYNET_PRODUCTION_API_URL')
-PAYNET_PRODUCTION_PAYNETJS_URL = os.getenv('PAYNET_PRODUCTION_PAYNETJS_URL')
+PAYNET_PUBLISHABLE_KEY = env('PAYNET_PUBLISHABLE_KEY', default='string for prevent crash building docker')
+PAYNET_SECRET_KEY = env('PAYNET_SECRET_KEY', default='string for prevent crash building docker')
+PAYNET_TEST_API_URL = env('PAYNET_TEST_API_URL', default='string for prevent crash building docker')
+PAYNET_TEST_PAYNETJS_URL = env('PAYNET_TEST_PAYNETJS_URL', default='string for prevent crash building docker')
+PAYNET_PRODUCTION_API_URL = env('PAYNET_PRODUCTION_API_URL', default='string for prevent crash building docker')
+PAYNET_PRODUCTION_PAYNETJS_URL = env('PAYNET_PRODUCTION_PAYNETJS_URL', default='string for prevent crash building docker')
 
 
--- docker_clean.sh
+++ docker_clean.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # set the nevironment for digital-ocean
-source ~/.bashrc
-dvm use 1.10.3
-docker -v
-eval $(docker-machine env istebu-core01)
+
+eval $(docker-machine env istebu)
 # Stop and remove all containers
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
