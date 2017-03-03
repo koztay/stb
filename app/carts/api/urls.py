433a958ead6111e4babe53c5349239400540487e
koztay <koztay@me.com>
2017-03-03 19:42:24 +0300

    cart summary sadece remove dışında tamamdır.

--- app/carts/api/urls.py
+++ app/carts/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CartItemsListView
 
 urlpatterns = [
-    url(r'^$', CartItemsListView.as_view(), name='api_cart_list'),  # /api/cart/
+    url(r'^$', CartItemsListView.as_view(), name='api_cartitems_list'),  # /api/cart/
 
 ]
 
